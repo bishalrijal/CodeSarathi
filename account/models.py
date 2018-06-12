46c06f687a963f0d7dfcf49cd662edaaf1b09461
Bishal Rijal <bishalrijal110@gmail.com>
2018-06-12 21:16:41 +0545

    migration error fixed

--- account/models.py
+++ account/models.py
@@ -4,6 +4,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+
+
 ##for programming languages
 class Languages(models.Model):
     name=models.CharField(max_length=200,blank=True, null=True,unique=True)
@@ -34,11 +36,11 @@ class Profile(models.Model):
         return 'profile of {}'.format(self.user.username)
 
 class Mentor(models.Model):
-    profile=models.OneToOneField(User,on_delete=models.SET_NULL,related_name="mentor_user", null=True ,default=" ",)
+    profile=models.OneToOneField(User,on_delete=models.SET_NULL, null=True)
     photos=models.ImageField(upload_to='mentor/%Y/%m/%d',blank=True,)
     bio = models.CharField(max_length=200,blank=True, null=True)
-    skill = models.ManyToManyField(TechSkill,related_name='mentor_skill')
-    languages=models.ManyToManyField(Languages,related_name='mentor_language')
+    skill = models.ManyToManyField(TechSkill,related_name='account_skill')
+    languages=models.ManyToManyField(Languages,related_name='account_language')
 
     def __str__(self):
         return '{}'.format(self.profile.username)
@@ -68,16 +70,13 @@ def save_user_profile(sender,instance,**kwargs):
     instance.profile.save()
 """
 #-----------------------------------------------------------------------------------------------------blog
-from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 from django.urls import reverse
 #from taggit.managers import TaggableManager
-
+print("hello")
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager,self).get_queryset()\
-            .filter(status='published')
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 
 class BlogPost(models.Model):
     def get_absolute_url(self):
